44645d07cb4d8a62f7faef37db848d707e6e766c
Spatialflunky1 <jfandrus84@gmail.com>
2022-10-08 00:01:03 -0400

    button test

--- watch/watch.ino
+++ watch/watch.ino
@@ -10,22 +10,14 @@
 // Initialize clock type
 RTC_DS3231 rtc;
 
-int LeftButtonState=0;
-int SelectButtonState=0;
-int RightButtonState=0;
 int year;
 int month;
 int day;
 int hour;
 int minute;
 int second;
-int posX;
 bool pm;
-
-void button_test() {
-  // Buttons in the list are: {Left, Select, Right}
-  int buttons[] = {digitalRead(2), digitalRead(3), digitalRead(4)};
-}
+int count = 0;
 
 void Clear_Screen() {
   LCD_Clear(0xffff);
@@ -35,23 +27,20 @@ void Clear_Screen() {
 void setup() {
   Serial.begin(9600);
 
-  // Position at which to display the time at
-  posX = (LCD_WIDTH/2)-(8*6);
+  // if (! rtc.begin()) {
+  //   Serial.println("Couldn't find RTC");
+  //   Serial.flush();
+  //   while (1) delay(10);
+  // }
 
-  if (! rtc.begin()) {
-    Serial.println("Couldn't find RTC");
-    Serial.flush();
-    while (1) delay(10);
-  }
-
-  if (rtc.lostPower()) {
-    // Going to do some time adjustment madness with this code in the future
-    Serial.println("RTC lost power, let's set the time!");
-    rtc.adjust(DateTime(F(__DATE__), F(__TIME__)));
-    // This line sets the RTC with an explicit date & time, for example to set
-    // January 21, 2014 at 3am you would call:
-    // rtc.adjust(DateTime(2014, 1, 21, 3, 0, 0));
-  }
+  // if (rtc.lostPower()) {
+  //   // Going to do some time adjustment madness with this code in the future
+  //   Serial.println("RTC lost power, let's set the time!");
+  //   rtc.adjust(DateTime(F(__DATE__), F(__TIME__)));
+  //   // This line sets the RTC with an explicit date & time, for example to set
+  //   // January 21, 2014 at 3am you would call:
+  //   // rtc.adjust(DateTime(2014, 1, 21, 3, 0, 0));
+  // }
 
   // Initialize buttons
   pinMode(2, INPUT);
@@ -62,29 +51,46 @@ void setup() {
   Config_Init();
   LCD_Init();
   Clear_Screen();
+
+  // Print initial count 
+  Paint_DrawNum((LCD_WIDTH/2)-(8*6),LCD_HEIGHT/2,count,&Font20,WHITE,BLACK);
 }
 
 void loop() {
+  int buttons[3] = {digitalRead(2), digitalRead(3), digitalRead(4)};
+  Serial.print(buttons[0]);
+  Serial.print(buttons[2]);
+  Serial.print("");
   // Gets the current time and date from the rtc
-  DateTime now = rtc.now();
-  year = now.year();
-  month = now.month();
-  day = now.day();
-  hour = now.hour();
-  minute = now.minute();
-  second = now.second();
-  Serial.println("second:");
-  Serial.println(second);
+  // DateTime now = rtc.now();
+  // year = now.year();
+  // month = now.month();
+  // day = now.day();
+  // hour = now.hour();
+  // minute = now.minute();
+  // second = now.second();
+  // Serial.println("second:");
+  // Serial.println(second);
   
+  // // Checks if in the morning or afternoon
+  // if (hour > 12) pm=true;
+  // else pm=false;
 
-  // Checks if in the morning or afternoon
-  if (hour > 12) pm=true;
-  else pm=false;
-
-  if (pm) hour-=12;
+  // if (pm) hour-=12;
   
-  PAINT_TIME current = {year,month,day,hour,minute,second};
-  Paint_DrawTime(posX,LCD_HEIGHT/2,&current,&Font20,WHITE,BLACK);
-  delay(750);
-  Paint_DrawTime(posX,LCD_HEIGHT/2,&current,&Font20,WHITE,WHITE);
+  // PAINT_TIME current = {year,month,day,hour,minute,second};
+  // Paint_DrawTime((LCD_WIDTH/2)-(8*6),LCD_HEIGHT/2,&current,&Font20,WHITE,BLACK);
+  // delay(750);
+  // Paint_DrawTime((LCD_WIDTH/2)-(8*6),LCD_HEIGHT/2,&current,&Font20,WHITE,WHITE);
+
+  if (buttons[0] == HIGH) {
+    Paint_DrawNum((LCD_WIDTH/2)-(8*6),LCD_HEIGHT/2,count,&Font20,WHITE,WHITE);
+    count -=1;
+    Paint_DrawNum((LCD_WIDTH/2)-(8*6),LCD_HEIGHT/2,count,&Font20,WHITE,BLACK);
+  }
+  if (buttons[2] == HIGH) {
+    Paint_DrawNum((LCD_WIDTH/2)-(8*6),LCD_HEIGHT/2,count,&Font20,WHITE,WHITE);
+    count +=1;
+    Paint_DrawNum((LCD_WIDTH/2)-(8*6),LCD_HEIGHT/2,count,&Font20,WHITE,BLACK);
+  }
 }
